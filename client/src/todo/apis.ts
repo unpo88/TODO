c79cb18e30eca316b002b0c5d374069f132aa67d
Whatsup <whatsup@lemonbase.com>
2024-04-12 17:05:40 +0900

    feat: API 편하게 사용하도록 변경

--- client/src/todo/apis.ts
+++ client/src/todo/apis.ts
@@ -1,20 +1,23 @@
 import axios from "axios";
 import { ITodo } from "@client/todo/types/todo.ts";
 
+const API_BASE_URL = "http://localhost:8000/api";
+
+
 function TodoApi() {
   async function getTodos() {
-    const response = await axios.get("http://localhost:8000/api/todo/");
+    const response = await axios.get(`${API_BASE_URL}/todo/`);
     const responseData: ITodo[] = response.data;
 
     return responseData;
   }
 
   async function createTodo(name: string) {
-    await axios.post("http://localhost:8000/api/todo/", { name: name });
+    await axios.post(`${API_BASE_URL}/todo/`, { name: name });
   }
 
   async function deleteTodo(entityId: string) {
-    await axios.delete(`http://localhost:8000/api/todo/${entityId}`);
+    await axios.delete(`${API_BASE_URL}/todo/${entityId}`);
   }
 
   async function updateTodo(
@@ -22,7 +25,7 @@ function TodoApi() {
     name: string,
     completed: boolean,
   ) {
-    await axios.put(`http://localhost:8000/api/todo/${entityId}/`, {
+    await axios.put(`${API_BASE_URL}/todo/${entityId}/`, {
       name: name,
       completed: completed,
     });
